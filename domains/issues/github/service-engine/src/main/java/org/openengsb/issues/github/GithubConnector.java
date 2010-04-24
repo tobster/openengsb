196d632cc9e7513a23b824bfb8c3da503be89e69
Tobias Hochwallner <tobster@gmx.at>
2010-04-24 15:39:40 +0200

    added licence

--- domains/issues/github/service-engine/src/main/java/org/openengsb/issues/github/GithubConnector.java
+++ domains/issues/github/service-engine/src/main/java/org/openengsb/issues/github/GithubConnector.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openengsb.issues.github;
 
 import java.util.Map;
@@ -41,7 +57,7 @@ public class GithubConnector implements IssuesDomain {
     public void deleteIssue(String id) {
         try {
             this.githubService.changeState(this.repositoryUser, this.project, Long.valueOf(id),
-                    GithubService.STATESCLOSE);
+                    GithubService.STATECLOSE);
         } catch (NumberFormatException e) {
             log.error("could not transform id >" + id + "<to long value", e);
         }
